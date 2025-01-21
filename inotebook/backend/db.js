4f9ea3f0f713c5eae8e6382ed0fa26b1068244a4
pasang tengin lama <pasang201722@gamil.com>
2025-01-21 23:35:36 +0545

    completion of 43  learned nodemon, and connection to database

---
+++ inotebook/backend/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/mydatabase', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error.message);
+  }
+};
+
+module.exports = connectToMongo;
+
+
+
+
+
